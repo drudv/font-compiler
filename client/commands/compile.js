a40b16e32e24ecb7aaee9d4651b58ca53cfa9fd0
Dmitry Druganov <drud@drud.cz>
2017-07-18 01:37:34 +0200

    Import commands from 'commands/' directory

---
+++ client/commands/compile.js
@@ -0,0 +1,5 @@
+
+export const command = 'compile';
+export const describe = 'Generates webfonts and templates';
+export const builder = {};
+export const handler = (argv) => {};
---
+++ client/commands/config.js
@@ -0,0 +1,5 @@
+
+export const command = 'config';
+export const describe = 'Generates a starter configuration file (fontcustom.yml)';
+export const builder = {};
+export const handler = (argv) => {};
